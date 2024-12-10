91953e79862415e0550944f1776d280193a432c5
Nelson Ramirez <xhyskoax@gmail.com>
2024-12-10 17:54:14 -0300

    configuración vercel

--- api/settings.py
+++ api/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-=cldztbc4jg&xl0!x673!*v2_=p$$eu)=7*f#d0#zs$44xx-h^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.vercel.app']
 
 
 # Application definition
@@ -131,7 +131,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles_build", "static") 
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles_build") 
 STATIC_URL = "/staticfiles/" 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
